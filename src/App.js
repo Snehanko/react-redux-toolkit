0a3d978caa385c33a27b777a323a39dc550442c3
Darx <snehankobose31@gmail.com>
2024-03-25 08:34:10 +0530

    Create a Todo Store using redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Todos from "./components/Todos";
+import AddTodos from "./components/AddTodos";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-3xl text-gray-600 bg-orange-500">
+      <AddTodos />
+      <Todos />
     </div>
   );
 }
